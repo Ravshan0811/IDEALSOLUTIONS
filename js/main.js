84cd388421bf1a0784f8a3ec09bf3b5b214f685a
Ravshan <oravshan26@gmail.com>
2022-12-21 00:01:17 +0500

    00:02 time

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 let btn = document.querySelector(".nav__img");
 let nav = document.querySelector(".nav");
+let ul = document.querySelector(".nav__list1");
+let li = document.querySelector(".nav__iteam");
 
 btn.addEventListener("click", () => {
   nav.classList.toggle("hidden");
+});
+
+li.addEventListener("click", () => {
+  ul.classList.toggle("hidden");
 });
